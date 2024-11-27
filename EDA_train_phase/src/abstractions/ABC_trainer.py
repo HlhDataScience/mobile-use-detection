1a8bc7f35bf63a36f664b2cc5bafb26d2374383e
Héctor López Hidalgo <data_analitics_HLH@protonmail.com>
2024-11-27 22:47:26 +0100

    Update ABC_trainer.py

--- EDA_train_phase/src/abstractions/ABC_trainer.py
+++ EDA_train_phase/src/abstractions/ABC_trainer.py
@@ -1,6 +1,10 @@
 """This module holds the abstract class for the train and test. It is thought to be instanciated and wotk seemlessly with the other classes defined in this project."""
 
 from abc import ABC, abstractmethod
+from EDA_train_phase.src.abstractions.ABC_interfaces import (
+IConfigModel,
+IConfigurationLoader,
+)
 
 
 class BasicTrainer(ABC):
@@ -9,8 +13,26 @@ class BasicTrainer(ABC):
     This i the second part of the module and follows the instantiation of BasicPipeline class
     """
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        config_model: IConfigModel,
+        config_loader: IConfigurationLoader,
+        config_path: str,
+        config_name: str,
+        config_section: str = None,
+    ):
+        self.config_model = config_model
+        self.config_data = config_loader.load(config_path, config_name)
+        
+        if config_section:
+            if config_section in self.config_data:
+                self.config_data = self.config_data[config_section]
+            else:
+                logging.error(f"Config section '{config_section}' not found.")
+                raise KeyError(
+                    f"Section '{config_section}' not found in the configuration."
+                )
+
 
     @abstractmethod
     def train(self):
